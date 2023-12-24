744d366901ce713a897f738223474e39fc27629a
vladaf <vladimir@gojilabs.com>
2023-12-24 04:22:40 -0500

    Basic chat

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,24 @@
+class ChatsController < ApplicationController
+  def index 
+    @chats = Chat.all
+  end
+
+  def new
+    @chat = Chat.new
+  end
+
+  def create
+    @chat = Chat.new(chat_params)
+    if @chat.save 
+      redirect_to chats_path
+    else 
+      render :new
+    end
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:name)
+  end
+end
