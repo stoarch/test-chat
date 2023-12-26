8e77d19d267cd6167a93b6fc5577398686346d4c
vladaf <vladimir@gojilabs.com>
2023-12-26 11:33:03 -0500

    Sending only to self

--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -2,8 +2,11 @@ import consumer from "channels/consumer"
 
 consumer.subscriptions.create({ channel: "ChatChannel", chat_id: 1 }, {
   received(data) {
-    const messages = document.getElementById('messages');
-    messages.innerHTML += data.message.length + "> " + data.message;
+    const currentUserId = document.body.getAttribute('data-user-id');
+    if(currentUserId !== data.user.toString()){
+      const messages = document.getElementById('messages');
+      messages.innerHTML += data.message.length + "> " + data.message;
+    }
 
     console.log("Message:",data, data.message);
   }
